6f213c00c343feb9166e8d7f992f9a4385acb03f
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-01-28 04:11:11 -0500

    Add CreatedModifiedDocMixIn

--- solarsan/models.py
+++ solarsan/models.py
@@ -1,5 +1,25 @@
 
+from datetime import datetime
 import mongoengine as m
+
+
+"""
+Document MixIns
+"""
+
+
+class CreatedModifiedDocMixIn(object):
+    meta = {'abstract': True}
+    created = m.DateTimeField(default=datetime.utcnow())
+    modified = m.DateTimeField(default=datetime.utcnow())
+
+    def save(self, *args, **kwargs):
+        """Overrides save for created and modified properties"""
+        if not self.pk:
+            self.created = datetime.utcnow()
+        if self._changed_fields:
+            self.modified = datetime.utcnow()
+        return super(CreatedModifiedDocMixIn, self).save(*args, **kwargs)
 
 
 """
@@ -7,7 +27,5 @@ Config
 """
 
 
-class Config(m.DynamicDocument):
+class Config(CreatedModifiedDocMixIn, m.DynamicDocument):
     name = m.StringField(unique=True)
-    #created = m.DateTimeField()
-    #modified = m.DateTimeField()
