a0048553c11aede4b0da9d12105f9ee533fd3dfa
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-01-28 21:10:13 -0500

    Update

--- dist/install.sh
+++ dist/install.sh
@@ -1,21 +1,27 @@
-#!/bin/bash
+#!/bin/bash -e
 
 export HOME="/root"
 export WORKON_HOME="/root/.virtualenvs"
 
+set +e
 . /etc/bash.bashrc
 . /etc/bash_completion
+set -e
 
 if [[ ! -e "/root/.virtualenvs/solarsan" ]]; then
     mkvirtualenv --no-site-packages --distribute -v solarsan
 fi
 
+set +e
 workon solarsan
 for i in /opt/solarsanweb /opt/solarsanweb/lib /opt/solarsan /opt/solarsan/solarsan; do
     add2virtualenv | egrep "^$i\$" >/dev/null 2>&1 || \
         add2virtualenv "$i"
 done
+set -e
 
+pip install -r /opt/solarsan/dist/requirements/dev-trevorj.pip
 
 # This is what tells us we don't need to be installed by salt
+echo "[$0] Success!"
 touch /opt/solarsan/.installed
