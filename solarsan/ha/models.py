8d3b5c3731dac8230bb98fc3b1fe139c69cc1dbe
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-02-04 01:12:23 -0500

    Add Peer Still Offline Reminder

--- solarsan/ha/models.py
+++ solarsan/ha/models.py
@@ -3,6 +3,8 @@
 import mongoengine as m
 from solarsan.models import CreatedModifiedDocMixIn, ReprMixIn
 from cluster.models import Peer
+import sh
+from netifaces import interfaces
 
 
 class ActivePassiveIP(CreatedModifiedDocMixIn, ReprMixIn, m.Document):
@@ -11,3 +13,21 @@ class ActivePassiveIP(CreatedModifiedDocMixIn, ReprMixIn, m.Document):
     netmask = m.StringField()
     iface = m.StringField()
     peer = m.ReferenceField(Peer, dbref=False)
+
+    @property
+    def iface_name(self):
+        return '%s:%s' % (self.iface, self.name)
+
+    #def __init__(self, *args, **kwargs):
+    #    super(ActivePassiveIP, self).__init__(*args, **kwargs)
+    #    self.is_active = self.check_status()
+
+    @property
+    def is_active(self):
+        return self.iface_name in interfaces()
+
+    def ifup(self):
+        sh.ifup(self.iface_name)
+
+    def ifdown(self):
+        sh.ifdown(self.iface_name)
