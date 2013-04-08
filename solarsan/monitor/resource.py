2031fd3a0cd68d5a180d50f6df117ad755e5bd4a
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-04-08 03:28:05 -0400

    Lets not allow nulls actually

--- solarsan/monitor/resource.py
+++ solarsan/monitor/resource.py
@@ -32,8 +32,8 @@ class ResourceManager(Component):
             self.add_res(res)
 
     def add_res(self, res):
-        if res.name in self.resources \
-        or None in res.peers:
+        if res.name in self.resources:
+        #or None in res.peers:
             return
         logger.info("Monitoring Resource '%s'.", res.name)
         self.resources[res.name] = res
