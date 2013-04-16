dad071a15d14e97de4946b73fe98b1c9ee1a1674
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-04-16 01:31:13 -0400

    Slight lun management from cli

--- solarsan/target/cli.py
+++ solarsan/target/cli.py
@@ -2,6 +2,8 @@
 from solarsan.conf import config
 from solarsan.cli.backend import AutomagicNode
 from solarsan.ha.models import FloatingIP
+from solarsan.storage.volume import Volume
+from solarsan.storage.drbd import DrbdResource
 from .models import iSCSITarget, SRPTarget
 
 """
@@ -52,6 +54,14 @@ class TargetNode(AutomagicNode):
 
         self.define_config_group_param('target', 'floating_ip', 'string', 'Floating IP associated with this Target')
 
+        self.define_config_group_param('luns', '0', 'string', 'Lun 0 device')
+        self.define_config_group_param('luns', '1', 'string', 'Lun 1 device')
+        self.define_config_group_param('luns', '2', 'string', 'Lun 2 device')
+        self.define_config_group_param('luns', '3', 'string', 'Lun 3 device')
+        self.define_config_group_param('luns', '3', 'string', 'Lun 3 device')
+        self.define_config_group_param('luns', '4', 'string', 'Lun 4 device')
+        self.define_config_group_param('luns', '5', 'string', 'Lun 5 device')
+
     def ui_getgroup_target(self, key):
         '''
         This is the backend method for getting keys.
@@ -80,6 +90,32 @@ class TargetNode(AutomagicNode):
             self.obj.floating_ip = fip
         else:
             return setattr(self.obj, key, value)
+
+    def ui_getgroup_luns(self, key):
+        key = int(key)
+        ret = None
+        if key >= 0 and key < len(self.obj.devices):
+            ret = self.obj.devices[key]
+        if ret:
+            return ret.name
+
+    def ui_setgroup_luns(self, key, value):
+        key = int(key)
+
+        res = None
+        vol = None
+
+        try:
+            res = DrbdResource.objects.get(name=value)
+        except DrbdResource.DoesNotExist:
+            vol = Volume(value)
+
+        if res:
+            self.obj.devices[key] = res
+        elif vol.exists():
+            self.obj.devices[key] = vol.name
+        else:
+            raise ValueError('Could not find replicated resource or Volume named "%s"' % value)
 
     def ui_command_save(self):
         self.obj.save()
