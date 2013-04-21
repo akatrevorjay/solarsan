49fde8659d07343cbda0aece624882ec1bf69f85
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-04-20 21:23:43 -0400

    Cleanup

--- solarsan/monitor/dkv.py
+++ solarsan/monitor/dkv.py
@@ -2,7 +2,7 @@
 from solarsan import logging
 logger = logging.getLogger(__name__)
 from circuits import Component, Event, Timer
-
+import pickle
 # Temp hack
 from _dev.zmq.clonecli import SUBTREE, Clone
 
@@ -13,11 +13,15 @@ Distributed Key Value Store Manager
 
 
 class DkvGet(Event):
-    """Sets a value in the distributed key value store"""
+    """Gets a value in the distributed key value store"""
 
 
 class DkvSet(Event):
     """Sets a value in the distributed key value store"""
+
+
+class DkvShow(Event):
+    """Shows a value in the distributed key value store"""
 
 
 def get_dkv_client():
@@ -31,18 +35,9 @@ def get_dkv_client():
 class DkvManager(Component):
     channel = 'dkv'
 
-    ##run_every = 300.0
-    #run_every = 10.0
-
     def __init__(self, channel=channel):
         super(DkvManager, self).__init__(channel=channel)
         self.clone = get_dkv_client()
-
-        #self._run_timer = Timer(self.run_every,
-        #                        LogWatchRun(),
-        #                        self.channel,
-        #                        persist=True,
-        #                        ).register(self)
 
     def dkv_get(self, key):
         return self.clone.get(key)
@@ -53,17 +48,15 @@ class DkvManager(Component):
     def dkv_set(self, key, value, ttl=0):
         return self.clone.set(key, value, ttl=ttl)
 
-    #def started(self, *args, **kwargs):
-    #    self.fire(LogWatchRun())
+    def started(self, *args, **kwargs):
+        self.fire(DkvSet('/test/trevorj', 'woot'))
 
-    #    clone['trevorj_yup'] = 'fksdkfjksdf'
-    #    clone[SUBTREE + 'trevorj'] = 'woot'
-    #    clone[SUBTREE + 'trevorj-pickle'] = pickle.dumps({'whoa': 'yeah', 'lbh': True})
+        clone = self.clone
 
-    #    logger.debug('SHOW SERVER: %s', clone.show('SERVER'))
-    #    logger.debug('SHOW SERVERS: %s', clone.show('SERVERS'))
-    #    logger.debug('SHOW SEQ: %s', clone.show('SEQ'))
+        clone['/test/trevorj_yup'] = 'fksdkfjksdf'
+        clone['/test/trevorj2'] = 'woot'
+        clone['/test/trevorj-pickle'] = pickle.dumps({'whoa': 'yeah', 'lbh': True})
 
-
-    #def log_watch_run(self):
-    #    self.monlog.run_once()
+        logger.debug('SHOW SERVER: %s', clone.show('SERVER'))
+        logger.debug('SHOW SERVERS: %s', clone.show('SERVERS'))
+        logger.debug('SHOW SEQ: %s', clone.show('SEQ'))
