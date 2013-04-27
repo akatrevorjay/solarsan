1a4f1a7eac822431326599352245e6eefe5e6d2b
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-04-27 01:42:06 -0400

    Reallly fix it

--- solarsan/logs/watch.py
+++ solarsan/logs/watch.py
@@ -12,15 +12,18 @@ class BaseMongoLogWatcher(object):
     def __init__(self, last=0):
         self._last_count = last
 
-        for x in xrange(last):
-            x = last - (x + 1)
-            try:
-                self._last = Syslog.objects.order_by('-pk')[last]
-                break
-            except Syslog.DoesNotExist:
-                pass
+        if last == 0:
+            self._last = Syslog.objects.order_by('-pk')[last]
+        else:
+            for x in xrange(last):
+                x = last - (x + 1)
+                try:
+                    self._last = Syslog.objects.order_by('-pk')[last]
+                    break
+                except Syslog.DoesNotExist:
+                    pass
 
-    def __iter__(self):
+    def next_chunk(self):
         try:
             ret = Syslog.objects.filter(pk__gt=self._last.pk)
 
@@ -34,13 +37,16 @@ class BaseMongoLogWatcher(object):
             logger.error("Monlog collection is invalid, ie is not capped. Dropping existing collection to re-initialize as such: %s", e)
             Syslog.drop_collection()
 
+    def __iter__(self):
+        return iter(self.next_chunk())
+
 
 class MongoLogWatcher(BaseMongoLogWatcher):
     class signals:
         check_log_entry = signals.check_log_entry
 
     def run_once(self):
-        for log in self():
+        for log in self.next_chunk():
             if self._debug:
                 logger.debug('Checking monlog: %s', log)
             self._check(log)
