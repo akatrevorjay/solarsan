f1e347a1fbe64d474fcb1ee6ab9c7346c8ea2f8d
Trevor Joynson (trevorj) <trevorj@skywww.net>
2013-04-30 05:37:42 -0400

    WIP

--- bin/reloader.py
+++ bin/reloader.py
@@ -8,32 +8,41 @@ feedback.
 Or to restart a daemon when configuration or data changes.
 Or any other use, the sky is the limit :)
 
-Usage: reloader.py [-p <path>] [-a <action] [-a <ignorelist>] [-s <sleep>] <command>
+Usage: reloader.py [-p <path>] [-i <ignore>] [-r <regex>] [-R <iregex>] [-s <sleep>] [-d] -- <command>...
 
-    -p <path> --path=<path>                     Path to monitor for changes. [default: current directory]
-    -a <action> --action=<action>               Action to perform when changes are detected. [default: restart]
-    -i <ignorelist> --ignorelist=<ignorelist>   Files to ignore.
-    -s <sleep> --sleep=<sleep>                  Ignore events for this many seconds after the last restart [default: 10]
-    <command>                                   Command to run and restart
+    -p <path> --path=<path>             Path to monitor for changes. [default: current directory]
+    -i <ignore> --ignore=<ignore>       Files to ignore. (unix glob match) [default: .*.swp,.git]
+    -r <regex> --regex=<regex>          Files to ignore. (regex)
+    -R <iregex> --iregex=<iregex>       Files to ignore. (regex case insensitive)
+    -s <sleep> --sleep=<sleep>          Ignore events for this many seconds after the last restart [default: 10]
+    -d --debug                          Debug [default: False]
+    <command>                           Command to run and restart
 
 """
 import logging
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 from docopt import docopt
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileMovedEvent, FileModifiedEvent
 from subprocess import Popen
 from time import time, sleep
+from fnmatch import fnmatch
 import re
+import os
 
 
 class RestartHandler(FileSystemEventHandler):
-    def __init__(self, command, path, ignorelist, focus, sleeptime, **kwargs):
+    _debug = False
+
+    def __init__(self, command, path, ignore, ignore_re, ignore_ire, sleep_time, debug, **kwargs):
         super(RestartHandler, self).__init__(**kwargs)
         self.command = command
-        self.ignorelist = ignorelist
-        self.focus = focus
-        self.sleep = sleeptime
+        self.ignore = ignore
+        self.ignore_re = ignore_re
+        self.ignore_ire = ignore_ire
+        self.sleep = sleep_time
+        self._debug = debug
         self.start()
 
     def stop(self):
@@ -47,18 +56,32 @@ class RestartHandler(FileSystemEventHandler):
         log.info('Started %s' % self._process)
 
     def on_any_event(self, event):
-        #if not isinstance(event, (FileMovedEvent, FileModifiedEvent)):
+        if self._debug:
+            log.debug('event=%s', event)
+
         if not isinstance(event, (FileModifiedEvent, )):
             return
 
         if self.sleep and time() < self._last_restart + self.sleep:
             return
 
-        for i in self.ignorelist:
-            r = re.compile('^' + i.replace('*', '.*') + '$')
-            if r.match(event.src_path):
+        for pat in self.ignore:
+            if fnmatch(event.src_path, pat) or isinstance(event, FileMovedEvent) and fnmatch(event.dest_path, pat):
                 return
-            if isinstance(event, FileMovedEvent) and r.match(event.dest_path):
+
+        def do_regex(pat, flags=0):
+            #r = re.compile('^' + i.replace('*', '.*') + '$')
+            r = re.compile(pat, flags=flags)
+            if r.match(event.src_path) or isinstance(event, FileMovedEvent) and r.match(event.dest_path):
+                return False
+            return True
+
+        for pat in self.ignore_re:
+            if not do_regex(pat):
+                return
+
+        for pat in self.ignore_ire:
+            if not do_regex(pat, re.IGNORECASE):
                 return
 
         log.warning('Restarting due to %s', event)
@@ -67,34 +90,44 @@ class RestartHandler(FileSystemEventHandler):
         self.start()
 
 
-def main(command, path, action, focus, sleeptime, ignorelist=None):
-        if action == 'restart':
-            ev = RestartHandler(command, path=path, focus=focus,
-                                sleeptime=sleeptime,
-                                ignorelist=ignorelist)
-        else:
-            raise NotImplementedError('action %s not implemented' % action)
+def main(arguments):
+    log.debug('args=%s', arguments)
+    command = arguments['<command>']
+    path = arguments['--path']
+    if path == 'current directory':
+        path = os.curdir
+    sleep_time = int(arguments['--sleep'])
+    ignore = arguments['--ignore']
+    ignore_re = arguments['--regex']
+    if ignore_re is None:
+        ignore_re = []
+    ignore_ire = arguments['--iregex']
+    if ignore_ire is None:
+        ignore_ire = []
+    debug = arguments['--debug']
 
-        ob = Observer()
-        ob.schedule(ev, path=path, recursive=True)
-        ob.start()
-        try:
-            while True:
-                sleep(1)
-        except KeyboardInterrupt:
-            ob.stop()
-        ob.join()
+    ev = RestartHandler(command,
+                        path=path,
+                        ignore=ignore,
+                        ignore_re=ignore_re,
+                        ignore_ire=ignore_ire,
+                        sleep_time=sleep_time,
+                        debug=debug,
+                        )
+
+    ob = Observer()
+    ob.schedule(ev, path=path, recursive=True)
+    ob.start()
+
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        ob.stop()
+
+    ob.join()
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Reloader 0.1')
-    kwargs = {}
-    for k, v in arguments.iteritems():
-
-        k = k.replace('--', '')
-        kwargs[k] = v
-
-    print(arguments)
-
-    #main(args.command, args.path, args.action, args.focus, args.sleep,
-    #     args.ignorelist)
+    main(arguments)
