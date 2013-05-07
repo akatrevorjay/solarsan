31d12344f0f2502cd1d2c0494ec82154679828b0
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2013-05-07 16:13:27 -0400

    Merge dkvcli into dkvclient

--- solarsan/zeromq/dkvclient.py
+++ solarsan/zeromq/dkvclient.py
@@ -517,3 +517,43 @@ class DkvAgent(object):
                            server_state, server.address)
                 self.cur_server = (self.cur_server + 1) % len(self.servers)
                 self.state = self.STATES.INITIAL
+
+
+def get_client(debug=True, discovery=True, connect_localhost=True, subtree=None):
+    """Create and connect dkv"""
+    return DkvClient(debug=debug, discovery=discovery, connect_localhost=connect_localhost, subtree=subtree)
+
+
+def _test(dkv):
+    from .encoders import pickle
+    SUBTREE = ''
+    dkv['trevorj_yup'] = 'fksdkfjksdf'
+    dkv[SUBTREE + 'trevorj'] = 'woot'
+    dkv[SUBTREE + 'trevorj-pickle'] = pickle.dumps(
+        {'whoa': 'yeah', 'lbh': True})
+
+    logger.debug('SHOW SERVER: %s', dkv.show('SERVER'))
+    logger.debug('SHOW SERVERS: %s', dkv.show('SERVERS'))
+    logger.debug('SHOW SEQ: %s', dkv.show('SEQ'))
+
+
+def _test_rand_cache(dkv):
+    import random
+    # Distribute as key-value message
+    key = "%d" % random.randint(1, 10000)
+    value = "%d" % random.randint(1, 1000000)
+    dkv.set(key, value, random.randint(0, 30))
+
+
+def main():
+    dkv = get_client()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
